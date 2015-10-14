dbdb52b43afc2a3ec738acc9815f3c45f416bc97
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-10-14 11:36:40 +0200

    create/login scripts w.i.p.

--- browser.py
+++ browser.py
@@ -17,49 +17,49 @@ class BrowserSimulator():
         self.driver = webdriver.Firefox()
 
     def go_to(self, url):
-        print "Going to %s. " % url
+        print u"Going to %s. " % url
         self.driver.get(url)
 
     def fill_in(self, name, text):
-        print "Writing '%s' in textarea named '%s'." % (text, name)
+        print u"Writing '%s' in textarea named '%s'." % (text, name)
         e = self.driver.find_element_by_name(name)
         e.clear()
         e.send_keys(text)
 
     def click_button(self, name):
-        print "Clicking button named '%s'." % name
+        print u"Clicking button named '%s'." % name
         e = self.driver.find_element_by_name(name)
         e.click()
 
     def click_link(self, id):
-        print "Clicking link '%s'." % id
+        print u"Clicking link '%s'." % id
         e = self.driver.find_element_by_id(id)
         e.click()
 
     def get_title(self):
-        print "Title is '%s'." % self.driver.title
+        print u"Title is '%s'." % self.driver.title
         return self.driver.title
 
     def get_text(self, id):
         e = self.driver.find_element_by_id(id)
         text = e.text
-        print "Element '%s' text is '%s'." % (id, text)
+        print u"Element '%s' text is '%s'." % (id, text)
         return text
 
     def verify_title(self, title):
         def condition():
             return self.get_title() == title
-        wait_for(condition, "Expected title to show up: " + title)
+        wait_for(condition, u"Expected title to show up: " + title)
 
     def verify_text(self, id, text):
         def condition():
             return text in self.get_text(id)
-        wait_for(condition, "Expected text to show up: " + text)
+        wait_for(condition, u"Expected text to show up: " + text)
 
     def verify_text_gone(self, id, text):
         def condition():
             return text not in self.get_text(id)
-        wait_for(condition, "Expected text to disappear: " + text)
+        wait_for(condition, u"Expected text to disappear: " + text)
 
     def close_browser(self):
         print "Closing browser."
--- systest.py
+++ systest.py
@@ -10,30 +10,34 @@ TESTACCOUNT = "test@test.com"
 TESTPASSWORD = "testtest123"
 
 
+def info(s):
+    print "\n%s" % s.upper()
+    print "=" * len(s)
+
+
 def test_end_to_end(url, headless=True):
     try:
-        print "Regression script running."
+        info("Regression script running.")
         browser = BrowserSimulator(headless)
 
-        # first clear out test data via backend testing API
-        # /deltestdata clears data for test@test.com email
-        # TODO: how avoid hard-coding backend endpoint
-        # elegantly?
+        info('Clear old test data')
         browser.go_to('http://localhost:5000/deltestdata')
 
-        # now start script on landing page
+        info("Going to landing page")
         browser.go_to(url)
         browser.verify_title('Re:Mind')
 
-        # Create test account
+        info("Create test account")
         create_test_account(browser)
 
-        # Login to test account
+        info("Login with test account")
+        login_to_test_account(browser)
 
-        # Run regression script
+        info("Run basic usage scenario")
         run_basic_usage_scenario(browser)
 
         # Delete test account
+        # delete_test_account(browser)
 
         print "##############################"
         print "### ALL DONE SCRIPT PASSED ###"
@@ -43,26 +47,44 @@ def test_end_to_end(url, headless=True):
     except Exception, e:
         print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         print "!!! Attention! Regression script didn't finish! Exception:"
-        print e
+        print unicode(e.message).encode('utf-8')
 
 
 def create_test_account(browser):
-    browser.click_link("signup")
-    browser.verify_title("Skapa konto")
-    browser.fill_in('email', TESTACCOUNT)
-    browser.fill_in('password', TESTPASSWORD)
-    browser.click_button('signup')
+
+    def create_flow():
+        browser.click_link("signup")
+        browser.verify_title("Skapa konto")
+        browser.fill_in('email', TESTACCOUNT)
+        browser.fill_in('password', TESTPASSWORD)
+        browser.click_button('signup')
+
+    create_flow()
     browser.verify_text("message", "Konto skapat")
-    browser.verify_title("Logga in")
+
+    info('Verify not possible re-create same account')
+    create_flow()
+    browser.verify_text("message", "Kontot finns redan!")
+
+
+def login_to_test_account(browser):
+    browser.click_link('login')
+
+    info('Verify wrong p/w does not login')
+    browser.fill_in('email', TESTACCOUNT)
+    browser.fill_in('password', TESTPASSWORD * 2)
+    browser.click_button("login")
+    browser.verify_text('message',
+                        u"Fel epost eller lösenord. Försök igen.")
+
+    info('Now login with correct p/w')
+    browser.fill_in('password', TESTPASSWORD)
+    browser.click_button("login")
+    browser.verify_text('message', u'Välkommen!')
+    browser.verify_title('Collect')
 
 
 def run_basic_usage_scenario(browser):
-    # use test email adress for whole script
-    browser.fill_in('email', 'test@test.com')
-
-    # go to collect page
-    browser.click_button('collect')
-    browser.verify_title('Collect')
 
     # enter & save some data
     browser.fill_in('note', 'hej')
@@ -91,4 +113,4 @@ if __name__ == '__main__':
         print "Expected a single argument 'URL', aborting."
     else:
         url = sys.argv[1]
-        test_end_to_end(url, headless=True)
+        test_end_to_end(url, headless=False)
